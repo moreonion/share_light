44e54c05b4bd2be9d09b5dc7d0eedcf049cc45c2
Katharina Zwinger <katharina@more-onion.com>
2017-03-01 12:01:23 +0100

    Remove redundant tags from mail template

--- share-message-body.tpl.php
+++ share-message-body.tpl.php
@@ -10,19 +10,16 @@
  * http://www.anandgraves.com/html-email-guide#effective_layout
  */
 ?>
-<html>
-  <body>
-    <table width="400px" cellspacing="0" cellpadding="10" border="0">
-      <tbody>
-        <tr>
-          <td style="font-family:Arial,Helvetica,sans-serif; font-size:12px;">
-            <?php if ($values['message']): ?>
-            <p><?php print t('Message from Sender'); ?></p><p><?php print $values['message']; ?></p>
-            <?php endif; ?>
-            <?php echo $values['footer']; ?>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </body>
-</html>
+
+<table width="400px" cellspacing="0" cellpadding="10" border="0">
+  <tbody>
+    <tr>
+      <td style="font-family:Arial,Helvetica,sans-serif; font-size:12px;">
+        <?php if ($values['message']): ?>
+        <p><?php print t('Message from Sender'); ?></p><p><?php print $values['message']; ?></p>
+        <?php endif; ?>
+        <?php echo $values['footer']; ?>
+      </td>
+    </tr>
+  </tbody>
+</table>
