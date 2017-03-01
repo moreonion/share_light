9beb8f5f328fddb55b919926880677e4a308d809
Roman Zimmermann <roman@more-onion.com>
2017-03-01 16:03:29 +0100

    Add comments for the tests.

--- tests/EmailTest.php
+++ tests/EmailTest.php
@@ -31,6 +31,9 @@ class EmailWebTest extends DrupalWebTestCase {
     }
   }
 
+  /**
+   * If the share path doesn't point to a node use the current node.
+   */
   public function testRenderWithNonNodePath() {
     $node = $this->drupalCreateNode(array('type' => 'page'));
     $block = new Block(['node' => $node, 'link' => ['path' => 'path']]);
@@ -40,21 +43,29 @@ class EmailWebTest extends DrupalWebTestCase {
     $this->assertEqual('path', $render['query']['path']);
   }
 
+  /**
+   * Node paths in the block configuration should override the current node.
+   */
   public function testRenderWithNodePath() {
+    // Share path points to the same node.
     $node = $this->drupalCreateNode(array('type' => 'page'));
-    $block = new Block(['node' => $node, 'link' => ['path' => "node/{$node->nid}"]]);
+    $path = "node/{$node->nid}";
+    $block = new Block(['node' => $node, 'link' => ['path' => $path]]);
     $email = new Email($block);
     $render = $email->render();
     $this->assertEqual("node/{$node->nid}/share", $render['href']);
+    // The share path points to the share node. No need to pass the path.
     $this->assertEqual([], $render['query']);
 
+    // Share path points to another node.
     $node2 = $this->drupalCreateNode(array('type' => 'page'));
-    $block = new Block(['node' => $node, 'link' => ['path' => "node/{$node2->nid}"]]);
+    $path = "node/{$node2->nid}";
+    $block = new Block(['node' => $node, 'link' => ['path' => $path]]);
     $email = new Email($block);
     $render = $email->render();
     $this->assertEqual("node/{$node2->nid}/share", $render['href']);
+    // The share path points to the share node. No need to pass the path.
     $this->assertEqual([], $render['query']);
-    $this->assertEqual($path, $render['query']['path']);
   }
 
 }
