435f63f15053bdbed27397c2f0b9e29c36443120
Michael Strasser <mst1409@gmx.at>
2018-04-05 20:51:38 +0200

    Add `mailto:` sharing to EMail channel

--- src/ChannelBase.php
+++ src/ChannelBase.php
@@ -47,4 +47,84 @@ abstract class ChannelBase implements ChannelInterface {
   public function enabled() {
     return !empty($this->options['toggle']);
   }
+
+  /**
+   * Fetches the node object we want to share.
+   *
+   * In case of `campaigninon_thankyou_page`s, the parent node
+   * will be loaded (e.g. the corresponding petition).
+   *
+   * @return object
+   *   An array containing the node object and the URL query.
+   */
+  protected function getNode() {
+    $node = $this->block->getNode();
+    $link = $this->block->getLink();
+    $query['path'] = $link['path'];
+    if (isset($link['query'])) {
+      $query['query'] = $link['query'];
+    }
+    if ($p_node = menu_get_object('node', 1, $query['path'])) {
+      $node = $p_node;
+    }
+    if ($node && $query['path'] == 'node/' . $node->nid) {
+      unset($query['path']);
+    }
+
+    return array(
+      'node' => $node,
+      'query' => $query,
+    );
+  }
+
+  /**
+   * Generates the URL to be shared.
+   *
+   * @param string $utm_source
+   *   The `utm_source` query parameter's value (e.g. `twitter_share`).
+   * @param object|null $node
+   *   The `node` object containing the `node`'s `nid`. If `NULL` is
+   *   provided, the node will be fetched via `getNode()`.
+   *
+   * @return string|null
+   *   The generated URL or NULL if no node could be fetched.
+   */
+  protected function generateShareUrl($utm_source, $node = NULL) {
+    if (!$node) {
+      $node = $this->getNode()['node'];
+    }
+
+    if ($node) {
+      $url = $this->block->getUrl();
+      $url .= strpos($url, '?') ? '&' : '?';
+      $url .=
+        'utm_source=' . $utm_source . '&utm_campaign=[' .
+        $node->nid . ']&utm_medium=share';
+
+      return $url;
+    }
+  }
+
+  /**
+   * Generates data needed for token replacement.
+   *
+   * @param string $utm_source
+   *   The `utm_source` query parameter's value for `[share:url]`
+   *   (e.g. `twitter_share`).
+   *
+   * @return array|null
+   *   The token replacement data, contaning the corresponding `node`
+   *   and `share` data.
+   *   Or NULL if no node could be fetched.
+   */
+  protected function generateTokenData($utm_source) {
+    $nq = $this->getNode();
+    $node = $nq['node'];
+    $url = $this->generateShareUrl($utm_source, $node);
+
+    if ($url) {
+      return ['node' => $node, 'share' => ['url' => $url]];
+    }
+  }
+
 }
--- src/Email.php
+++ src/Email.php
@@ -18,22 +18,39 @@ class Email extends ChannelBase {
   }
 
   /**
+   * Returns the default values for the channel.
+   */
+  public static function defaults() {
+    return [
+      'mailto_toggle' => FALSE,
+      'mailto' =>
+      [
+        'subject' => '[node:title]',
+        'body' => '[share:url]',
+      ],
+    ] + parent::defaults();
+  }
+
+  /**
    * Renders the HTML element.
    */
   public function render() {
-    $node = $this->block->getNode();
-    $link = $this->block->getLink();
-    $query['path'] = $link['path'];
-    if (isset($link['query'])) {
-      $query['query'] = $link['query'];
+    if ($this->options['mailto_toggle']) {
+      return $this->renderLink();
     }
-    if ($p_node = menu_get_object('node', 1, $query['path'])) {
-      $node = $p_node;
-    }
+
+    return $this->renderForm();
+  }
+
+  /**
+   * Renders an HTML email form..
+   */
+  private function renderForm() {
+    $nq = $this->getNode();
+    $node = $nq['node'];
+    $query = $nq['query'];
+
     if ($node) {
-      if ($query['path'] == 'node/' . $node->nid) {
-        unset($query['path']);
-      }
       if ($query) {
         $query['hash'] = static::signQuery($query);
       }
@@ -51,6 +68,37 @@ class Email extends ChannelBase {
   }
 
   /**
+   * Returns a link field containing a `mailto:` URI.
+   *
+   * @return array|null
+   *   The link field's renderable array or NULL if the generation
+   *   of token replacement data failed.
+   */
+  private function renderLink() {
+    $data = $this->generateTokenData('email_share');
+
+    if ($data) {
+      $query['subject'] = htmlspecialchars_decode(
+        token_replace($this->options['mailto']['subject'], $data),
+        ENT_QUOTES);
+      $query['body'] = htmlspecialchars_decode(
+        token_replace($this->options['mailto']['body'], $data),
+        ENT_QUOTES);
+
+      return [
+        'title' => 'Email',
+        'href' => 'mailto:',
+        'query' => $query,
+        'attributes' => [
+          'title' => t('Share this via Email!'),
+          'data-share' => 'email',
+        ],
+        'external' => TRUE,
+      ];
+    }
+  }
+
+  /**
    * Generates a base64 encoded HMAC.
    *
    * @return string
@@ -61,4 +109,43 @@ class Email extends ChannelBase {
     $key = drupal_get_hash_salt() . __FILE__;
     return drupal_hmac_base64(serialize($query), $key);
   }
+
+  /**
+   * Adds a widget containing the options of the `Email` channel.
+   *
+   * Allows the user to choose between sharing via an online form
+   * or the local Email client via a `mailto:` URI.
+   */
+  public static function optionsWidget(array &$element, array $options) {
+    $title = static::title();
+    $ctoggle_id = drupal_html_id('share-light-channel-' . $title . '-mailto');
+    $element['mailto_toggle'] = [
+      '#type' => 'checkbox',
+      '#title' => t("Use mail:to link for email sharing via the users' email client"),
+      '#description' => t("Enables the user to send an email via the users' local email client instead of a webform"),
+      '#attributes' => ['id' => $ctoggle_id],
+      '#default_value' => $options['mailto_toggle'],
+      '#value_callback' => 'email_mailto_checkbox_value',
+    ];
+    $element['mailto'] = [
+      '#type' => 'container',
+      '#states' => ['visible' => ['#' . $ctoggle_id => ['checked' => TRUE]]],
+    ];
+    $element['mailto']['subject'] = [
+      '#title' => t('Subject'),
+      '#description' => t("The email's subject line."),
+      '#type' => 'textfield',
+      '#size' => 60,
+      '#default_value' => $options['mailto']['subject'],
+    ];
+    $element['mailto']['body'] = [
+      '#title' => t('Body'),
+      '#description' => t("The email's body."),
+      '#type' => 'textarea',
+      '#cols' => 60,
+      '#rows' => 5,
+      '#default_value' => $options['mailto']['body'],
+    ];
+  }
+
 }
